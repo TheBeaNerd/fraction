36fc3550d9a1c256885c7cf02a5d06bcefc6b558
Dave <TheBeaNerd@gmail.com>
2019-08-27 22:28:19 -0500

    header

--- generic-mod-property.lisp
+++ generic-mod-property.lisp
@@ -1,3 +1,11 @@
+;; 
+;; Copyright (C) 2019, David Greve
+;; All rights reserved.
+;; 
+;; This software may be modified and distributed under the terms of
+;; the 3-clause BSD license.  See the ACL2 LICENSE file for details.
+;; 
+;;
 (in-package "ACL2")
 
 (encapsulate
