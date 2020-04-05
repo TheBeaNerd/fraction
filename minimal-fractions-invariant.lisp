d077f496c78fa36f3bb0ae81bc75a41c42e72f22
David Greve <TheBeaNerd@gmail.com>
2020-04-04 19:31:05 -0500

    improved lt-sqrt

--- minimal-fractions-invariant.lisp
+++ minimal-fractions-invariant.lisp
@@ -238,11 +238,11 @@
          (natp m)
          (natp x)
          (non-trivial-modulus q)
-         (generic-invertible-p x q)
+         ;;(generic-invertible-p x q)
          (smallest-coefficient-pair k m x q)
          (posp a)
-         (< k q)
-         (< m q)
+         ;;(<= k q)
+         ;;(<= m q)
          (<= (- (nmod (* k x) q)) (pmod (* m x) q))
          )
         (smallest-coefficient-pair-p a k (+ k m) x q))
@@ -265,11 +265,11 @@
          (natp m)
          (natp x)
          (non-trivial-modulus q)
-         (generic-invertible-p x q)
+         ;;(generic-invertible-p x q)
          (smallest-coefficient-pair k m x q)
          (posp a)
-         (< k q)
-         (< m q)
+         ;;(<= k q)
+         ;;(<= m q)
          (< (pmod (* m x) q) (- (nmod (* k x) q)))
          )
         (smallest-coefficient-pair-p a (+ k m) m x q))
@@ -294,10 +294,10 @@
       (natp m)
       (natp x)
       (non-trivial-modulus q)
-      (generic-invertible-p x q)
+      ;;(generic-invertible-p x q)
       (smallest-coefficient-pair k m x q)
-      (< k q)
-      (< m q)
+      ;;(<= k q)
+      ;;(<= m q)
       (<= (- (nmod (* k x) q)) (pmod (* m x) q)))
      (smallest-coefficient-pair k (+ k m) x q))
     :hints (("Goal" :in-theory (disable pfix)
@@ -313,10 +313,10 @@
       (natp m)
       (natp x)
       (non-trivial-modulus q)
-      (generic-invertible-p x q)
+      ;;(generic-invertible-p x q)
       (smallest-coefficient-pair k m x q)
-      (< k q)
-      (< m q)
+      ;;(<= k q)
+      ;;(<= m q)
       (< (pmod (* m x) q) (- (nmod (* k x) q)))
       )
      (smallest-coefficient-pair (+ k m) m x q))
@@ -347,17 +347,19 @@
   (if (< p (- n)) (mv (+ k m) (+ n p) m p)
     (mv k n (+ k m) (+ n p))))
 
+(in-theory (disable mv-nth mv-nth-to-val))
+
 (defthm smallest-coefficient-pair-step-minimal-fractions-pair
   (implies
    (and
+    (smallest-coefficient-pair k m x q)
     (natp k)
     (natp m)
     (natp x)
     (non-trivial-modulus q)
-    (generic-invertible-p x q)
-    (< k q)
-    (< m q)
-    (smallest-coefficient-pair k m x q)
+    ;;(generic-invertible-p x q)
+    ;;(<= k q)
+    ;;(<= m q)
     (equal n (nmod (* k x) q))
     (equal p (pmod (* m x) q)))
    (mv-let (k n m p) (step-minimal-fractions-pair k n m p)
@@ -367,6 +369,98 @@
   :hints (("Subgoal 1" :use (smallest-coefficient-pair-invariant-1))
           ("Subgoal 2" :use (smallest-coefficient-pair-invariant-2))))
 
+;; (defun minimal-invariant (k n m p q)
+;;   (and (equal (* k p) (+ q (* m n)))
+;;        (implies
+;;         (<= -1 n)
+;;         (<= 1 k))
+;;        (implies
+;;         (<= p 0)
+;;         (and (< k q)
+;;              (equal m 1)))
+;;        (implies
+;;         (<= p 1)
+;;         (<= 1 m))))
+
+;; dag
+;; (encapsulate
+;;     ()
+
+;;   (local (include-book "arithmetic-5/top" :dir :system))
+  
+;;   (SET-NON-LINEARP T)
+  
+;;   (defthmd invariant-induced-bound
+;;     (implies
+;;      (and
+;;       (minimal-invariant k n m p q)
+;;       (natp m)
+;;       (natp k)
+;;       (negp n)
+;;       (natp p)
+;;       (posp q))
+;;      (and (<= k q)
+;;           (< m q))))
+  
+;;     :hints (("Goal" :cases ((<= p 0)))))
+
+;;   )
+
+;; (defthmd minimal-invariant-step-minimal-fractions-pair
+;;   (implies
+;;    (and
+;;     (minimal-invariant k n m p q)
+;;     (natp k)
+;;     (negp n)
+;;     (natp m)
+;;     (natp p)
+;;     (posp q))
+;;    (mv-let (k n m p) (step-minimal-fractions-pair k n m p)
+;;      (minimal-invariant k n m p q))))
+
+;; (defthm NON-TRIVIAL-MODULUS-implies-posp
+;;   (implies
+;;    (NON-TRIVIAL-MODULUS x)
+;;    (posp x))
+;;   :rule-classes (:forward-chaining))
+
+;; (defthm smallest-coefficient-pair-step-minimal-fractions-pair
+;;   (implies
+;;    (and
+;;     (smallest-coefficient-pair k m x q)
+;;     (natp k)
+;;     (natp m)
+;;     (natp x)
+;;     (non-trivial-modulus q)
+;;     (generic-invertible-p x q)
+;;     (minimal-invariant k n m p q)
+;;     (posp p)
+;;     (equal n (nmod (* k x) q))
+;;     (equal p (pmod (* m x) q)))
+;;    (mv-let (k n m p) (step-minimal-fractions-pair k n m p)
+;;      (and (smallest-coefficient-pair k m x q)
+;;           (minimal-invariant k n m p q)
+;;           (equal n (nmod (* k x) q))
+;;           (equal p (pmod (* m x) q)))))
+;;   :otf-flg t
+;;   :hints (("Goal" :in-theory '(NON-TRIVIAL-MODULUS-implies-posp
+;;                                T-T-IMPLIES-NEGP-NMOD
+;;                                T-T-IMPLIES-NATP-PMOD)
+;;            :use (invariant-induced-bound
+;;                  minimal-invariant-step-minimal-fractions-pair
+;;                  smallest-coefficient-pair-step-minimal-fractions-pair-bound))))
+
+;; #+joe
+;; (defthm smallest-coefficient-non-increasing
+;;   (implies
+;;    (and
+;;     (negp n)
+;;     (posp p))
+;;    (mv-let (k n1 m p1) (step-minimal-fractions-pair k n m p)
+;;      (declare (ignore k m))
+;;      (and (<= n n1)
+;;           (<= p1 p))))
+;;   :rule-classes :linear)
 
 (encapsulate
     ()
@@ -403,18 +497,18 @@
   (implies
    (and
     (natp k)
-    (integerp n)
-    (< n 0)
+    (negp n)
     (natp m)
     (natp p)
-    (equal (- (* k p) (* m n)) c))
+    (equal (* k p) (+ c (* m n))))
    (mv-let (k n m p) (step-minimal-fractions-pair k n m p)
-           (equal (- (* k p) (* m n)) c))))
+           (equal (* k p) (+ c (* m n))))))
 
 (encapsulate
     ()
 
   (defun lt-sqrt (x q)
+    (declare (type integer x q))
     (< (* x x) q))
      
   (local (include-book "arithmetic-5/top" :dir :system))
@@ -576,6 +670,49 @@
     :hints (("Goal" :in-theory '(num-equal prod)
              :use one-fraction-lt-sqrt-helper-2)))
 
+  (defthm equal-p--n
+    (implies
+     (and
+      (posp z)
+      (posp p)
+      (natp c)
+      (equal (* z p) c)
+      (not (lt-sqrt z c)))
+     (or (equal (* p p) c)
+         (lt-sqrt p c)))
+    :rule-classes nil
+    :hints (("Goal" :in-theory (enable lt-sqrt))))
+
+  (defthm lt-sqrt-negative
+    (equal (lt-sqrt (- x) y)
+           (lt-sqrt x y))
+    :hints (("Goal" :in-theory (enable lt-sqrt))))
+
+  (local (in-theory (disable NEGATIVE-LT-SQRT)))
+
+  (defthm one-fraction-lt-sqrt-alt
+    (implies
+     (and
+      (posp k)
+      (integerp n)
+      (< n 0)
+      (posp m)
+      (posp p)
+      (natp c)
+      (equal (- (* k p) (* m n)) c)
+      (lt-sqrt k c)
+      (lt-sqrt m c)
+      (not (lt-sqrt (+ k m) c)))
+     (or (equal (* p p) c)
+         (lt-sqrt p c)
+         (lt-sqrt n c)))
+    :otf-flg t
+    :rule-classes nil
+    :hints (("Goal" :do-not-induct t
+             :use ((:instance one-fraction-lt-sqrt
+                              (c (- (* k p) (* m n))))
+                   (:instance equal-p--n
+                              (z (+ k m)))))))
   )
 
 ;; k n m p
@@ -584,6 +721,33 @@
 ;; (equal (- (* 1 Q) (* 0 -v)) q)
 ;; 0 -Q 1 v
 ;; (equal (- (* 0 v) (* 1 -Q)) q)
+
+(defun-sk minimal-fractions-pair (k n m p x q)
+  (forall (z) (minimal-fractions-pair-p (pfix z) k n m p x q)))
+
+(defthm smallest-coefficient-pair-implies-minimal-fractions-pair
+  (implies
+   (and
+    (posp z)
+    (natp k)
+    (natp m)
+    (natp x)
+    (posp q)
+    (equal n (nmod (* k x) q))
+    (equal p (pmod (* m x) q))
+    (smallest-coefficient-pair k m x q))
+   (minimal-fractions-pair k n m p x q))
+  :hints (("Goal" :in-theory (e/d (smallest-coefficient-pair-implies-minimal-fractions-pair-p)
+                                  (nmod-reduction)))))
+  
+(defun minimal-fractions-pair-listp (list x q)
+  (if (not (consp list)) (null list)
+    (let ((entry (car list)))
+      (case-match entry
+        ((k n m p)
+         (and (minimal-fractions-pair k n m p x q)
+              (minimal-fractions-pair-listp (cdr list) x q)))
+        (& nil)))))
 
 (defun fractions-listp (list)
   (declare (type t list))
@@ -606,21 +770,92 @@
           (mv-let (k n m p) (step-minimal-fractions-pair k n m p)
             (minimal-fractions-list-rec k n m p)))))
 
-#+joe
+(in-theory (disable nmod-reduction step-minimal-fractions-pair ))
+
+(defthm minimal-fractions-pair-listp-minimal-fractions-list-rec
+  (implies
+   (and
+    (natp k)
+    (natp m)
+    (natp x)
+    (non-trivial-modulus q)
+    ;;(generic-invertible-p x q)
+    (smallest-coefficient-pair k m x q)
+    (equal n (nmod (* k x) q))
+    (equal p (pmod (* m x) q)))
+   (minimal-fractions-pair-listp (minimal-fractions-list-rec k n m p) x q))
+  :hints (("Goal" :induct (minimal-fractions-list-rec k n m p))
+          (and stable-under-simplificationp
+               '(:expand (:free (p n) (MINIMAL-FRACTIONS-LIST-REC K N M P))))))
+
 (def::un minimal-fractions-list (x q)
   (declare (xargs :signature ((integerp posp) fractions-listp)))
   (let ((x (pmod x q)))
-    (let ((g (xgcd x q)))
-      (if (not (equal g 1)) nil
-        (let ((k 0)
-              (n (- q))
-              (m 1)
-              (p x))
-          (minimal-fractions-list-rec k n m p))))))
+    (if (equal x 0) nil
+      (let ((k 0)
+            (n (- q))
+            (m 1)
+            (p x))
+        (minimal-fractions-list-rec k n m p)))))
 
-          
-;; (def::un minimal-fraction-rec (x q)
-;;   )
+(defthm nmod-zero
+  (equal (nmod 0 q) (- (pfix q)))
+  :hints (("Goal" :in-theory (enable nmod))))
+
+(defthm timez-zero
+  (equal (* 0 x) 0))
+
+(defthm minimal-fractions-pair-listp-minimal-fractions-list
+  (implies
+   (and
+    (natp x)
+    (non-trivial-modulus q)
+    ;;(generic-invertible-p x q)
+    )
+   (minimal-fractions-pair-listp (minimal-fractions-list x q) x q)))
+
+(defun print-minimal-fractions-pair-list (list)
+  (if (not (consp list)) (null list)
+    (let ((entry (car list)))
+      (case-match entry
+        ((k n m p)
+         (prog2$
+          (cw "~p1/~p0 ~p3/~p2~%" k n m p)
+          (print-minimal-fractions-pair-list (cdr list))))
+        (t nil)))))
+
+(in-theory (disable lt-sqrt))
+
+(def::un minimum-fraction-rec (k n m p q)
+  (declare (xargs :signature ((natp negp natp natp posp) integerp natp)
+                  :hints (("Goal" :in-theory (enable step-minimal-fractions-pair)))
+                  :guard (and (lt-sqrt k q) (lt-sqrt m q))
+                  :measure (+ (nfix (- (ifix n))) (nfix p))))
+  (if (not (and (< (ifix n) 0) (< 0 (nfix p)))) (mv p m)
+    (mv-let (k1 n1 m1 p1) (step-minimal-fractions-pair k n m p)
+      (if (and (lt-sqrt k1 q) (lt-sqrt m1 q))
+          (minimum-fraction-rec k1 n1 m1 p1 q)
+        (if (and (lt-sqrt n q) (lt-sqrt p q))
+            (let ((f1 (max k (- n)))
+                  (f2 (max m p)))
+              (if (< f1 f2) (mv n k)
+                (mv p m)))
+          (if (lt-sqrt n q)
+              (mv n k)
+            (mv p m)))))))
+
+#+joe
+(defthm lt-sqrt-minimum-fraction-rec
+  (implies
+   (and
+    (lt-sqrt k q)
+    (lt-sqrt m q)
+    )
+   (mv-let (n d) (minimum-fraction-rec k n m p q)
+     (and (lt-sqrt n)
+          (lt-sqrt d)))))
 
 ;; (def::un minimal-fraction (x q)
 ;;   )
+
+
